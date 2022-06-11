76c9eb434820d5d1df3bf52d703cd45ec9b57d4d
dmlee <gmgmgun@gmail.com>
2022-06-11 18:06:28 +0900

    220611 Office

--- GrimpanTest/ChildView.h
+++ GrimpanTest/ChildView.h
@@ -57,7 +57,7 @@ public:
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg void OnDrawCanvas();
 	void SaveFigure(Gdiplus::Point startPoint, Gdiplus::Point endPoint);
-	//void LoadFigure(Gdiplus::Point &startPoint, Gdiplus::Point &endPoint);
+	void LoadFigure(Gdiplus::Point &startPoint, Gdiplus::Point &endPoint);
 	//CList<Figure, Figure&> m_figureList;
 	int m_mode;
 	bool m_IsBtnDown;
